bf2a6fdbeacc265ed851d1c9f69b51c7733f0a1b
CommanderKeynes <andrewjackson947@gmail.com>
2023-07-14 16:28:56 -0500

    moved vm functionality from read me to CLI

--- cli.sh
+++ cli.sh
@@ -104,7 +104,89 @@ install_packages () {
 }
 
 
+setup_vm () {
+
+    VBoxManage createvm \
+        --name archvm \
+        --ostype Arch_64 \
+        --register
+    VBoxManage modifyvm archvm \
+        --firmware efi
+    VBoxManage modifyvm archvm \
+        --memory 8000
+    VBoxManage createhd \
+        --filename `pwd`/archvm/archvm_DISK.vdi \
+        --size 80000 \
+        --format VDI
+    VBoxManage storagectl archvm \
+        --name "SATA Controller" \
+        --add sata \
+        --controller IntelAhci
+    VBoxManage storageattach archvm \
+        --storagectl "SATA Controller" \
+        --port 0 \
+        --device 0 \
+        --type hdd \
+        --medium  `pwd`/archvm/archvm_DISK.vdi
+    VBoxManage storagectl archvm \
+        --name "IDE Controller" \
+        --add ide \
+        --controller PIIX4 
+    VBoxManage storageattach archvm \
+        --storagectl "IDE Controller" \
+        --port 1 \
+        --device 0 \
+        --type dvddrive \
+        --medium ~/Downloads/archlinux-2023.07.01-x86_64.iso
+    VBoxManage modifyvm archvm \
+        --boot1 dvd \
+        --boot2 disk \
+        --boot3 none \
+        --boot4 none 
+    VBoxManage modifyvm archvm \
+        --nic1 nat
+    VBoxManage modifyvm archvm \
+        --natpf1 "guestssh,tcp,,2222,,22"
+    VBoxManage startvm archvm \
+        --type headless
+
+}
+
+
 action=$1
+
+if [ "$action" = "creaate_vm" ]
+then 
+    setup_vm
+fi
+
+if [ "$action" = "ssh_to_vm" ]
+then
+    rm /home/andrew/.ssh/known_hosts
+    ssh -p 2222 root@127.0.0.1
+fi
+
+
+if [ "$action" = "detach_usb_from_vm" ]
+then
+    VBoxManage shutdown archvm
+    VBoxManage modifyvm archvm \
+        --boot1 disk \
+        --boot2 dvd \
+        --boot3 none \
+        --boot4 none
+    VBoxManage startvm archvm \
+        --type headless
+fi
+
+
+if [ "$action" = "destroy_vm" ]
+then
+    rm `pwd`/archvm/archvm_DISK.vdi 
+    VBoxManage shutdown archvm
+    VBoxManage unregistervm archvm --delete
+fi
+
 if [ "$action" = "build_iso" ]
 then 
     mkarchiso -v -w /tmp/archiso-tmp/ ./archlivve/
--- readme.md
+++ readme.md
@@ -4,52 +4,6 @@ This repo stores all of my dotfiles.
 
 ## Initial Setup
 
-Create VM
-```bash
-VBoxManage createvm \
-    --name archvm \
-    --ostype Arch_64 \
-    --register
-VBoxManage modifyvm archvm \
-    --firmware efi
-VBoxManage modifyvm archvm \
-    --memory 8000
-VBoxManage createhd \
-    --filename `pwd`/archvm/archvm_DISK.vdi \
-    --size 80000 \
-    --format VDI
-VBoxManage storagectl archvm \
-    --name "SATA Controller" \
-    --add sata \
-    --controller IntelAhci
-VBoxManage storageattach archvm \
-    --storagectl "SATA Controller" \
-    --port 0 \
-    --device 0 \
-    --type hdd \
-    --medium  `pwd`/archvm/archvm_DISK.vdi
-VBoxManage storagectl archvm \
-    --name "IDE Controller" \
-    --add ide \
-    --controller PIIX4 
-VBoxManage storageattach archvm \
-    --storagectl "IDE Controller" \
-    --port 1 \
-    --device 0 \
-    --type dvddrive \
-    --medium ~/Downloads/archlinux-2023.07.01-x86_64.iso
-VBoxManage modifyvm archvm \
-    --boot1 dvd \
-    --boot2 disk \
-    --boot3 none \
-    --boot4 none 
-VBoxManage modifyvm archvm \
-    --nic1 nat
-VBoxManage modifyvm archvm \
-    --natpf1 "guestssh,tcp,,2222,,22"
-VBoxManage startvm archvm \
-    --type headless
-```
 
 ssh into vm
 ```bash
