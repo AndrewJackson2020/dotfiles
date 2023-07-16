84c7506b89c6861960dd81af43aca77fa5659075
CommanderKeynes <andrewjackson947@gmail.com>
2023-07-16 13:48:20 -0500

    Refactored shell script some

--- cli.sh
+++ cli.sh
@@ -5,53 +5,60 @@ set -e
 source ./cli_source.sh
 
 
-if [ "$1" = "vm" ]
+if [ "$1" = "--help" ]
+then
+
+elif [ "$1" = "vm" ]
 then 
-	if [ "$2" = "create" ]
+	if [ "$2" = "--help" ]
+	    echo "Help for VM CLI"
+	elif [ "$2" = "create" ]
 	then 
 	    setup_vm
-	fi
 
-	if [ "$2" = "ssh" ]
+	elif [ "$2" = "ssh" ]
 	then
 	    rm --force /home/andrew/.ssh/known_hosts
-	    scp  -P 2222 ./andrew_arch_iso/airootfs/root/install_linux.sh root@127.0.0.1:/root/
-	    scp -r -P 2222 ./andrew_arch_iso/airootfs/root/archinstall_installer/archinstall_installer root@127.0.0.1:/root/archinstall_installer
+	    scp  \
+		-P 2222 \
+		./andrew_arch_iso/airootfs/root/install_linux.sh \
+		root@127.0.0.1:/root/
+	    scp -r \
+		-P 2222 \
+		./andrew_arch_iso/airootfs/root/archinstall_installer/archinstall_installer \
+		root@127.0.0.1:/root/archinstall_installer
 	    ssh -p 2222 root@127.0.0.1
-	fi
 
 
-	if [ "$2" = "detach_boot" ]
+	elif [ "$2" = "detach_boot" ]
 	then
 	    detach_usb_from_vm 
-	fi
 
-
-	if [ "$2" = "destroy" ]
+	elif [ "$2" = "destroy" ]
 	then
 	    rm `pwd`/archvm/archvm_DISK.vdi 
 	    VBoxManage shutdown archvm
 	    VBoxManage unregistervm archvm --delete
+	else
+	    echo "Command $2 not recognized."
 	fi
 
-fi
-
-if [ "$1" = "build_iso" ]
+elif [ "$1" = "build_iso" ]
 then 
     mkarchiso -v -w /tmp/archiso-tmp/ ./archlivve/
-fi
 
-if [ "$1" = "stow" ]
+elif [ "$1" = "stow" ]
 then
     stow_config_files
-fi
 
-if [ "$1" = "unstow" ]
+elif [ "$1" = "unstow" ]
 then
     unstow_config_files
-fi
 
-if [ "$1" = "install" ]
+elif [ "$1" = "install" ]
 then
     install_packages
+else 
+    echo "Command $1 not recognized"
 fi
+
