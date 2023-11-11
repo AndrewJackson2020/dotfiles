dcc1c1d4f2572d1046eb4ef6ded708815a6f7f5b
CommanderKeynes <andrewjackson947@gmail.com>
2023-11-11 17:34:22 -0600

    Implemented terraform config file distribution

--- config/main.tf
+++ config/main.tf
@@ -3,7 +3,7 @@
 # Sync system files
 resource "local_file" "system_files" {
     for_each = fileset("${path.module}/system", "**")
-    content = filebase64("${path.module}/system/${each.value}")
+    content_base64 = filebase64("${path.module}/system/${each.value}")
     filename = "/${each.value}"
 }
 
@@ -11,7 +11,7 @@ resource "local_file" "system_files" {
 # Sync home files
 resource "local_file" "home_files" {
     for_each = fileset("${path.module}/home", "**")
-    content = filebase64("${path.module}/home/${each.value}")
-    filename = "/home/andrew/{$each.value}"
+    content_base64 = filebase64("${path.module}/home/${each.value}")
+    filename = "/home/andrew/${each.value}"
 }
 
