8a47741cbc4dc3a8cc3685945bcb1ad6dd9a996a
CommanderKeynes <46945903+CommanderKeynes@users.noreply.github.com>
2023-11-14 18:27:56 -0600

    Built initial entrypoint to powershell CLI appliication

--- cli.ps1
+++ cli.ps1
@@ -1,5 +1,29 @@
+<#
+.SYNOPSIS
+CLI to assist in building Windows based VM's
 
+.DESCRIPTION
+USAGE
+    ./cli.ps1 <command>
 
-. ./cli/powershell/build_vm_hyper_v.ps1
-. ./cli/powershell/build_vm_virtualbox.ps1
+COMMANDS
+    hyperv      Commands related to managing HyperV VM's
+    virtualbox  Commands related to managing Virtualbox VM's
 
+#>
+param(
+  [Parameter(Position=0, Mandatory=$True)]
+  [ValidateSet("hyperv", "virtualbox")]
+  [string]$Command
+)
+
+switch ($Command) {
+    "hyperv"  {
+        . ./cli/powershell/build_vm_hyper_v.ps1
+        Write-Host hyperv
+    }
+    "virtualbox"  {
+        . ./cli/powershell/build_vm_virtualbox.ps1
+        Write-Host virtuaalbox
+    }
+}
