c32f05225def6981697daab8d50a632a6b903f2a
CommanderKeynes <andrewjackson947@gmail.com>
2024-03-03 22:55:11 -0600

    Cool

--- nix_home_manager/home.nix
+++ nix_home_manager/home.nix
@@ -1,8 +1,13 @@
-{ config, pkgs, ... }:
+# TODO Should build below by getting recursive list of files in home directory
+
+{ lib, config, pkgs, ... }:
 
 let
   inherit (pkgs)
     fetchzip
+  ;
+  inherit (lib)
+    mkMerge
   ;
   terraform_src = fetchzip {
     url = "https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_linux_amd64.zip";
@@ -12,6 +17,16 @@ let
     url = "https://go.dev/dl/go1.22.0.linux-amd64.tar.gz";
     hash = "sha256-gRj8ZbcTc7rK8jVDxi13izHcIbbwjFx9hS4GIm7l9ks=";
   };
+  get_dotfiles = x: map (y: x + "/" + y) (builtins.attrNames (lib.filterAttrs (n: v: v == "regular") (builtins.readDir (./home + x))));
+  home_file = x: ./home + ("/" + x);
+  files = builtins.concatLists [
+    (get_dotfiles "") 
+    (get_dotfiles "/.config/doom") 
+    (get_dotfiles "/.config/i3") 
+    (get_dotfiles "/.config/nvim")
+    (get_dotfiles "/.config/terminator")
+  ];
+  files_map = map (x: {"${x}".source = (home_file x);}) files;
 in
 {
   home.username = "andrew";
@@ -19,31 +34,12 @@ in
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # TODO Should build below by getting recursive list of files in home directory
-  # TODO figure out how to loop through values and assign home file mappings dynamically
 
-  home.file = {
-
-    ".local/bin/terraform".source = terraform_src; 
-    ".local/go/".source = go_src;
-
-    ".background".source = ./home/.background; 
-    ".bashrc".source = ./home/.bashrc; 
-    ".config/doom/config.el".source = ./home/.config/doom/config.el; 
-    ".config/doom/init.el".source = ./home/.config/doom/init.el; 
-    ".config/i3/config".source = ./home/.config/i3/config; 
-    ".config/nvim/init.lua".source = ./home/.config/nvim/init.lua; 
-    ".config/terminator/config".source = ./home/.config/terminator/config; 
-    ".fehbg".source = ./home/.fehbg; 
-    ".gitconfig".source = ./home/.gitconfig; 
-    ".psqlrc".source = ./home/.psqlrc; 
-    ".tmux.conf".source = ./home/.tmux.conf; 
-    ".tmux.conf.local".source = ./home/.tmux.conf.local; 
-    ".vimrc".source = ./home/.vimrc; 
-    ".xinitrc".source = ./home/.xinitrc; 
-    ".zshrc".source = ./home/.zshrc; 
-    "cli.sh".source = ./home/cli.sh; 
-  };
+  home.file = mkMerge ([
+    {".local/bin/terraform".source = terraform_src;} 
+    {".local/go".source = go_src;}
+  ] ++ files_map);
 
   programs.home-manager.enable = true;
 }
+
