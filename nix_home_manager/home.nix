3b96aca52b26a54b6d100b558f34ac39bc4064c8
CommanderKeynes <andrewjackson947@gmail.com>
2024-03-04 21:36:07 -0600

    Implemented recursive nix

--- nix_home_manager/home.nix
+++ nix_home_manager/home.nix
@@ -1,31 +1,20 @@
-# TODO Should build below by getting recursive list of files in home directory
 
 { lib, config, pkgs, ... }:
 
 let
-  inherit (pkgs)
-    fetchzip
-  ;
-  inherit (lib)
-    mkMerge
-  ;
-  terraform_src = fetchzip {
+  terraform_src = pkgs.fetchzip {
     url = "https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_linux_amd64.zip";
     hash = "sha256-eoPAIM4FtC0fAwW851ouiZpL8hkQ/whKanI26xOX+9M=";
   };
-  go_src = fetchzip {
+  go_src = pkgs.fetchzip {
     url = "https://go.dev/dl/go1.22.0.linux-amd64.tar.gz";
     hash = "sha256-gRj8ZbcTc7rK8jVDxi13izHcIbbwjFx9hS4GIm7l9ks=";
   };
-  get_dotfiles = x: map (y: x + "/" + y) (builtins.attrNames (lib.filterAttrs (n: v: v == "regular") (builtins.readDir (./home + x))));
+  get_files = x: map (y: x + "/" + y) (builtins.attrNames (lib.filterAttrs (n: v: v == "regular") (builtins.readDir (./home + x))));
+  get_directories = x: builtins.concatLists (map (y: get_dotfiles (x + "/" + y)) (builtins.attrNames (lib.filterAttrs (n: v: v == "directory") (builtins.readDir (./home + x)))));
+  get_dotfiles = x: map (y: y) (builtins.concatLists [(get_directories x) (get_files x)]);
   home_file = x: ./home + ("/" + x);
-  files = builtins.concatLists [
-    (get_dotfiles "") 
-    (get_dotfiles "/.config/doom") 
-    (get_dotfiles "/.config/i3") 
-    (get_dotfiles "/.config/nvim")
-    (get_dotfiles "/.config/terminator")
-  ];
+  files = get_dotfiles "";
   files_map = map (x: {"${x}".source = (home_file x);}) files;
 in
 {
@@ -34,8 +23,7 @@ in
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-
-  home.file = mkMerge ([
+  home.file = lib.mkMerge ([
     {".local/bin/terraform".source = terraform_src;} 
     {".local/go".source = go_src;}
   ] ++ files_map);
